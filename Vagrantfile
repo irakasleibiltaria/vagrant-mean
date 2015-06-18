ca64a344240b1254a93774e98922e95942931927
zmwebdev <zmwebdev@gmail.com>
2015-06-18 22:32:18 +0200

    +

--- Vagrantfile
+++ Vagrantfile
@@ -2,4 +2,7 @@ Vagrant.configure(2) do |config|
   config.vm.box = "hashicorp/precise64"
   config.vm.provision :shell, path: "bootstrap.sh"
   config.vm.network :forwarded_port, host: 3000, guest: 3000
+  config.vm.network :forwarded_port, host: 5858, guest: 5858
+  config.vm.network :forwarded_port, host: 8080, guest: 8080
+  #config.vm.network "private_network", ip: "192.168.50.4"
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
 
 apt-get update
+
+apt-get install -y curl build-essential
+curl -SLO http://nodejs.org/dist/v0.12.4/node-v0.12.4-linux-x64.tar.gz
+tar -C /usr/local --strip-components 1 -xzf node-v0.12.4-linux-x64.tar.gz
+rm node-v0.12.4-linux-x64.tar.gz
+
+npm install -g nodemon node-inspector
+#chown -R vagrant /home/vagrant/.npm
+
 ##apt-get install -y python-software-properties
 #apt-get install -y software-properties-common
 #add-apt-repository -y ppa:chris-lea/node.js
 #apt-get update
-#apt-get install -y nodejs npm
+#apt-get install -y nodejs
 
-apt-get install -y curl
-curl -sL https://deb.nodesource.com/setup | sudo bash -
-apt-get install -y nodejs
+#apt-get install -y curl
+#curl -sL https://deb.nodesource.com/setup | sudo bash -
+#apt-get install -y nodejs
 
 #npm install -g npm
 #npm cache clean -f
@@ -24,10 +33,12 @@ apt-get install -y nodejs
 #echo 'prefix = ~/.node' >> ~/.npmrc
 #echo 'export PATH=$HOME/.node/bin:$PATH' >> ~/.bashrc
 #echo 'export NODE_PATH=$NODE_PATH:$HOME/.node/lib/node_modules' >> ~/.bashrc
+#source ~/.bashrc
 
 #echo 'prefix = /home/vagrant/.node' >> /home/vagrant/.npmrc
-#echo 'export PATH=$HOME/.node/bin:$PATH' >> /home/vagrant/.bashrc
-#echo 'export NODE_PATH=$NODE_PATH:$HOME/.node/lib/node_modules' >> /home/vagrant/.bashrc
+#echo 'export PATH=/home/vagrant/.node/bin:$PATH' >> /home/vagrant/.bashrc
+#echo 'export NODE_PATH=$NODE_PATH:/home/vagrant/.node/lib/node_modules' >> /home/vagrant/.bashrc
+#source /home/vagrant/.bashrc
 
 # vagrant ssh; then 
 #npm install -g yo bower grunt-cli gulp
